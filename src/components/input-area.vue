61b3149fc9547583b30b3865ed18f3cfe44322fb
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2018-07-14 19:12:45 +0200

    Introduce input area

---
+++ src/components/input-area.vue
@@ -0,0 +1,34 @@
+<template>
+  <fieldset class="input-area">
+    <textarea
+      class="input-area__textarea"
+      ref="json"
+      @change='getJson'
+    ></textarea>
+    <label for="input-area__format-button" 
+           class="input-area__format-button-label">
+      <input 
+        class="input-area__format-button"
+        id="input-area__format-button" 
+        type="button" 
+        value="Format JavaScript Serialized Object Notation" />
+    </label>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'input-area',
+  methods: {
+    getJson: function () {
+      console.log(this.$refs.json.value);
+    }
+  }
+}
+</script>
+
+<style scoped type="text/scss">
+  @import '../styles/input-area.scss';
+</style>
+
+
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
-import Dico from './components/dictionary.vue';
+import Dictionary from './components/dictionary.vue';
+import InputArea from './components/input-area.vue';
 
 // eslint-disable-next-line
 const app = new Vue({
     el: '#data-structures',
     components: {
-        dictionary: Dico,
+        dictionary: Dictionary,
+        'input-area': InputArea,
     },
 });
 
