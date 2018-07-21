a80d7db00ebe073d25c29997251f0363afc08cd2
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2018-07-21 18:28:32 +0200

    Add content about precedence of operators

--- src/components/content/a-simple-syntax-directed-translator/precedence-of-operators.vue
+++ src/components/content/a-simple-syntax-directed-translator/precedence-of-operators.vue
@@ -1,6 +1,6 @@
 <template>
   <div class='precedence-of-operators'>
-    <multimedia-content>Consider the expression <expr>9+5 * 2</expr>.
+    <multimedia-content>Consider the expression <expr>9+5*2</expr>.
 There are two possible interpretations of this expression: <expr>(9+5)*2</expr>
 or <expr>9+(5*2)</expr>. The associativity rules for <expr>+</expr>
 and <expr>*</expr> apply to occurrences of the same operator,
@@ -8,9 +8,15 @@ so they do not resolve this ambiguity.
 Rules defining the relative precedence of operators are needed 
 when more than one kind of operator is present.
 <br />
-We say that * has higher
-precedence than + if * takes its operands before +
-does. In ordinary arithmetic, multiplication and division have higher precede11ce than addition and subtraction. Therefore, 5 is taken by * in botl1 9+5*2 and 9*5+2; i.e., the expressions are equivalent to 9+ (5*2) and (9*5) +2, respectively.
+We say that <expr>*</expr> has higher
+precedence than <expr>+</expr> if <expr>*</expr>
+takes its operands before <expr>+</expr> does.
+In ordinary arithmetic, multiplication and division
+have higher precedence than addition and subtraction.
+Therefore, <expr>5</expr> is taken by <expr>*</expr>
+in both <expr>9+5*2</expr> and <expr>9*5+2</expr>; i.e.,
+the expressions are equivalent to <expr>9+(5*2)</expr> and </expr>(9*5)+2</expr>,
+respectively.
   </multimedia-content>
 </div>
 </template>
