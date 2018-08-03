5838593001f1d755f6fa5be941ef67170e7a2a40
Thierry Marianne <thierry.marianne@weaving-the-web.org>
2018-08-03 14:33:19 +0200

    Add test helpers to select buttons and tear down components

--- test/specs/test-helpers.js
+++ test/specs/test-helpers.js
@@ -5,7 +5,7 @@ Vue.config.productionTip = false;
 const destroyComponent = (component) => {
   let componentCandidate = component;
 
-  if (typeof componentCandidate === 'undefined') {
+  if (typeof componentCandidate === 'undefined' || componentCandidate === null) {
     return;
   }
 
@@ -29,6 +29,47 @@ const destroyComponent = (component) => {
   }
 };
 
+const shouldDestroyAfter = destroyAfter => (
+  typeof destroyAfter === 'undefined' || destroyAfter
+);
+
+const shouldDeletePreviousWrapper = destroyAfter => (
+  typeof destroyAfter !== 'undefined' && !destroyAfter
+);
+
+const ensureComponentWrapperIsDestroyedAfter = (context) => {
+  const caseContext = context;
+  if (shouldDestroyAfter(context.destroyAfter)) {
+    caseContext.wrappersForDeletion[context.wrapperForDeletionName] = caseContext.componentWrapper;
+  }
+
+  if (shouldDeletePreviousWrapper(caseContext.destroyAfter)) {
+    caseContext.wrappersForDeletion[context.wrapperForDeletionName] = undefined;
+    return {
+      subjectUnderTestWrapper: caseContext.componentWrapper,
+    };
+  }
+
+  return { subjectUnderTestWrapper: caseContext.componentWrapper };
+};
+
+const selectPairButtonAtIndex = (index) => {
+  const selector = '.editable-json .json__pair---button';
+  const matchingElements = document.querySelectorAll(selector);
+  if (typeof matchingElements[index] === 'undefined') {
+    throw Error(`No matching element for selector ${selector}`);
+  }
+
+  return matchingElements[index];
+};
+
+const selectFirstPairButton = () => (selectPairButtonAtIndex(0));
+
 export default {
   destroyComponent,
+  shouldDestroyAfter,
+  shouldDeletePreviousWrapper,
+  ensureComponentWrapperIsDestroyedAfter,
+  selectFirstPairButton,
+  selectPairButtonAtIndex,
 };
